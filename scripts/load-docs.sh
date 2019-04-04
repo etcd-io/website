52d0d9f6bb8156462a486a8cca7ab4e7ded001e0
lucperkins <lucperkins@gmail.com>
2019-04-04 12:28:45 -0700

    More updates to loading script
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- scripts/load-docs.sh
+++ scripts/load-docs.sh
@@ -8,10 +8,13 @@ ORIGINAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 RELEASES=$(cat ${ROOT}/RELEASES)
 
 for release in ${RELEASES}; do
+    release_target_dir=${ROOT}/content/docs/v${release}
     echo "Loading release v${release}"
     cd ${ROOT}/etcd
     git checkout v${release}
-    cp -rf Documentation ${ROOT}/content/docs/v${release}
+    cp -rf Documentation ${release_target_dir}
+    cp Documentation/docs.md ${release_target_dir}/_index.md
+    rm ${release_target_dir}/docs.md
 done
 
 cd ${ROOT}
